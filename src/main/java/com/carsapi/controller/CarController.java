d3fff6e616c52826bec01ac34f2d3e64a1b84545
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-17 11:35:51 -0300

    feat: add functionality to post endpoint

--- src/main/java/com/carsapi/controller/CarController.java
+++ src/main/java/com/carsapi/controller/CarController.java
@@ -1,14 +1,19 @@
 package com.carsapi.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carsapi.dto.CarDTO;
 
 @RestController
 @RequestMapping("/car")
 public class CarController {
   @PostMapping
-  public void create() {
-
+  public void create(@RequestBody CarDTO reqCarDTO) {
+    System.out.println("Adicionado carro:");
+    System.out.println(reqCarDTO.modelo());
+    System.out.println(reqCarDTO.anoModelo());
   }
 }

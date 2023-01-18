393ff329a3ed827f6f9b8173725a545bc0b46a4e
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-18 12:26:30 -0300

    feat: add persistance to post method and a get method

--- src/main/java/com/carsapi/controller/CarController.java
+++ src/main/java/com/carsapi/controller/CarController.java
@@ -1,12 +1,16 @@
 package com.carsapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carsapi.dto.CarDTO;
+import com.carsapi.model.Car;
 import com.carsapi.repository.CarRepository;
 
 @RestController
@@ -16,8 +20,14 @@ public class CarController {
   @Autowired
   private CarRepository repository;
 
+  @GetMapping
+  public List<Car> findAll() {
+    return repository.findAll();
+  }
+
   @PostMapping
   public void create(@RequestBody CarDTO reqCarDTO) {
+    repository.save(new Car(reqCarDTO));
     System.out.println("Adicionado carro:");
     System.out.println(reqCarDTO.modelo());
     System.out.println(reqCarDTO.anoModelo());

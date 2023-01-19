67c71d6d751a359495d68bed5a98c3d2ffd57cd4
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-19 15:42:11 -0300

    feat: add PUT method to controller

--- src/main/java/com/carsapi/controller/CarController.java
+++ src/main/java/com/carsapi/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,17 @@ public class CarController {
   @DeleteMapping("/{id}")
   public void delete(@PathVariable Long id) {
     repository.deleteById(id);
+  }
+
+  @PutMapping(value = "/{id}")
+  public void update(@PathVariable Long id, @RequestBody @Valid CarDTO reqCarDTO) {
+    repository.findById(id).map(car -> {
+      car.setModelo(reqCarDTO.modelo());
+      car.setFabricante(reqCarDTO.fabricante());
+      car.setAnoModelo(reqCarDTO.anoModelo());
+      car.setDataFabricacao(reqCarDTO.dataFabricacao());
+      car.setValor(reqCarDTO.valor());
+      return repository.save(car);
+    });
   }
 }
